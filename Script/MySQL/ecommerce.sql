1b8777def2f4ba1f51eadc0516dc74fa48f8f1db
vponugoti1 <vponugoti1@slu.edu>
2022-11-10 11:08:28 -0600

    basic springboot setup for sql

---
+++ Script/MySQL/ecommerce.sql
@@ -0,0 +1,3 @@
+drop database if exists ecommerce;
+
+CREATE DATABASE 'ecommerce' DEFAULT CHARACTER SET utf8;
---
+++ src/main/java/com/example/ecommerce/EcommerceServer.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServer.class, args);
+	}
+}
